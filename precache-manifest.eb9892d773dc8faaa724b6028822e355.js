cbdc382076e31b94b925d1d8ac93e58030dbe334
Diogo Perillo <diogo@mainframe.com>
2018-11-30 15:32:11 -0200

    Updates

--- precache-manifest.eb9892d773dc8faaa724b6028822e355.js
+++ precache-manifest.eb9892d773dc8faaa724b6028822e355.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/elections-dapp/static/js/runtime~main.79377f00.js"
   },
   {
-    "revision": "4cd874e6bfbfa8415fd9",
-    "url": "/elections-dapp/static/js/main.4cd874e6.chunk.js"
+    "revision": "7c6b8f5a96073778ceed",
+    "url": "/elections-dapp/static/js/main.7c6b8f5a.chunk.js"
   },
   {
     "revision": "4f0a804c045aa7878012",
     "url": "/elections-dapp/static/js/1.4f0a804c.chunk.js"
   },
   {
-    "revision": "4cd874e6bfbfa8415fd9",
+    "revision": "7c6b8f5a96073778ceed",
     "url": "/elections-dapp/static/css/main.a49b4a2c.chunk.css"
   },
   {
-    "revision": "50bb735872959f5dc2d8d356bb0910f2",
+    "revision": "a1551ba8f07fb0380156c2428df044c1",
     "url": "/elections-dapp/index.html"
   }
 ];
